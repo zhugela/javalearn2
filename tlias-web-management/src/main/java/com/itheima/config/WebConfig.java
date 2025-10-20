872d2aa6b43cb618a26b677b8bc790c93e0abf25
朱远 <zyuanliang971@gmail.com>
2025-10-20 14:54:44 +0800

    完成了自定义拦截器TOkenInterceptor的登录校验

--- tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
+++ tlias-web-management/src/main/java/com/itheima/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.itheima.config;
 
 import com.itheima.interceptor.DemoInterceptor;
+import com.itheima.interceptor.TokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,11 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
     //自定义的拦截器对象
     @Autowired
     private DemoInterceptor demoInterceptor;
-
+    //拦截器对象
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //注册自定义拦截器对象
         registry.addInterceptor(demoInterceptor).addPathPatterns("/**");//设置拦截器拦截的请求路径（ /** 表示拦截所有请求）
+
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
     }
 }
---
+++ tlias-web-management/src/main/java/com/itheima/interceptor/TokenInterceptor.java
@@ -0,0 +1,51 @@
+package com.itheima.interceptor;
+
+import com.itheima.utils.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1. 获取请求url。
+        String url = request.getRequestURL().toString();
+
+        //2. 判断请求url中是否包含login，如果包含，说明是登录操作，放行。
+        if(url.contains("login")){ //登录请求
+            log.info("登录请求 , 直接放行");
+            return true;
+        }
+
+        //3. 获取请求头中的令牌（token）。
+        String jwt = request.getHeader("token");
+
+        //4. 判断令牌是否存在，如果不存在，返回错误结果（未登录）。
+        if(!StringUtils.hasLength(jwt)){ //jwt为空
+            log.info("获取到jwt令牌为空, 返回错误结果");
+            response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        //5. 解析token，如果解析失败，返回错误结果（未登录）。
+        try {
+            JwtUtils.parseJWT(jwt);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("解析令牌失败, 返回错误结果");
+            response.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        //6. 放行。
+        log.info("令牌合法, 放行");
+        return true;
+    }
+
+}
